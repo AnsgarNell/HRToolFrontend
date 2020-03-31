532547e5ce6560acbb565c9c4244eb2b0a20a4e7
Ansgar Nell <ansgar.nell@gmail.com>
2020-03-31 23:29:20 +0200

    Added departments list component.

---
+++ src/app/departments/departments-list/departments-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../../shared/services/api.service';
+import {Departments} from '../departments-model';
+import {finalize} from 'rxjs/operators';
+import {MessageService} from '../../shared/services/message.service';
+
+@Component({
+  selector: 'app-departments-list',
+  templateUrl: './departments-list.component.html',
+  styleUrls: ['./departments-list.component.css']
+})
+export class DepartmentsListComponent implements OnInit {
+  departments: Departments[];
+  loading: boolean;
+
+  constructor(apiServicesService: ApiService,
+              private messageService: MessageService) {
+    this.loading = true;
+    apiServicesService.getDepartments()
+      .pipe(
+        finalize(() => this.loading = false),
+      ).subscribe(
+      departments => {
+        this.departments = departments;
+      },
+      error => {
+        this.messageService.add(`${error.name}: "${error.message}"`);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+  }
+
+}
